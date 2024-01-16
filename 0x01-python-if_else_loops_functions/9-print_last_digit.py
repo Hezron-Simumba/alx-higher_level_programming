59f66fd7cf379492b04f46a401ac0f67de5cc6cb
Hezron-Simumba <simumbahezron.com>
2024-01-16 23:13:53 +0200

    update 1

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    last = number % 10
-    print(last, end='')
-    return(last)
+  def print_last_digit(number):
+      if number < 0:
+         number = -number
+  last_digit = number % 10
+  print("{}".format(last_digit), end='')
+  return(last_digit)
